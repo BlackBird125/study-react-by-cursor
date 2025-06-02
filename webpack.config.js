21db4edc4c03bf4d6fdacdcbaedf43d79a8f2b97
大崎　康一朗 <KoichiroOsaki>
2025-06-02 14:26:54 +0900

    デバッグ環境の実装

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  devtool: "source-map",
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+};
